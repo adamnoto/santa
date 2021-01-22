d4437a53a6fbfbd0e08921b8f28b505ea0478d6d
Adam Notodikromo <adamnoto@hey.com>
2021-01-22 12:57:17 +0900

    basic rendering to ejs

--- server.js
+++ server.js
@@ -6,23 +6,39 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
+const { User } = require("./user");
 
+app.set("view engine", "ejs");
 app.use(bodyParser());
 app.use(morgan());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/views/index.html');
+  const accepted = request.app.get("accepted");
+  console.log("ACCEPTED? ", accepted);
+  response.render("pages/index");
 });
 
 app.post("/", (request, response) => {
-  response.send("Got a POST you sure?")
+  const userName = request.body.username;
+  const wish = request.body.wish;
+  console.log("User", User);
+  User.findByUsername(userName).then((user) => {
+    request.app.set("accepted", user.canReceivePresent());
+    response.redirect("/");
+  }).catch((reason) => {
+    // we may handle this and report error to centralized
+    // error system such as Sentry
+    console.log(reason);
+    request.app.set("accepted", false);
+    response.redirect("/");
+  })
 })
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
